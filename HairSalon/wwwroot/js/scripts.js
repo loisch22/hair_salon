fe625a64c73c27f66d9cd9c8d028ac6a5b801aa1
Lois Choi <loisch22@gmail.com>
2017-08-19 12:53:40 -0700

    try adding js

---
+++ HairSalon/wwwroot/js/scripts.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//   $(".start").click(function(event) {
+//     event.preventDefault();
+//     // $("#index").fadeOut();
+//     $("#stylist-form").fadeIn();
+//   });
+// });
